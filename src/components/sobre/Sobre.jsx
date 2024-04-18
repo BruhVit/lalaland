9d4ebd5574e373103bd36fd9c32ed43b996e4d6b
BruhVit <128320344+BruhVit@users.noreply.github.com>
2024-04-17 22:18:48 -0300

    Add files via upload

---
+++ src/components/sobre/Sobre.jsx
@@ -0,0 +1,19 @@
+import "./sobre.css"
+
+export const Sobre=({categoria}) =>{
+    return( 
+        <><div id={categoria}>
+            <div id="resumo">
+                <h2>Sobre mim</h2>
+                <p>Formada no colégio Firjan Sesi Senai, integrado com técnico de informática, participo de várias competições e projetos estudantis de nível nacional e internacional envolvendo robótica e criação de soluções inovadoras, tive vários destaques ao longo do ensino médio em competições que exigiam diferentes competências como montagem, programação, criação de vídeo, design, etc. Também participei de muitas olimpíadas do conhecimento como OBA, OBMEP, Canguru, ONC entre outras, fui campeã da Olimpíada do Bicentenário da Independência do Brasil pelo Estado do Rio de Janeiro e representei o Estado em Brasília, faço parte do Digital Youth Council programa da agência dos EUA e implementado pela MakingCents e seus parceiros, além de embaixadora da América Latina na campanha de mudanças climáticas HEY da Ashley Lashley Foundation.</p>
+                <h2>Meus conhecimentos</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>ReactJS</li>
+                    </ul>
+                </div></div></>
+
+    )
+}
